6f9723d1e586cc0a62e32d14679c701a6bb984b3
FrontendSa <laixingyu_a@163.com>
2022-03-08 11:32:18 +0800

    xxx

---
+++ src/components/School.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="demo">
+    <h2>学校地址：{{ name }}</h2>
+    <h2>学校地址：{{ address }}</h2>
+    <button @click="showName">点击提示学校名</button>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "School",
+  data() {
+    return {
+      name: "尚硅谷",
+      address: "北京",
+    };
+  },
+  methods: {
+    showName() {
+      alert(this.name);
+    },
+  },
+};
+</script>
+
+<style>
+.demo {
+  background-color: orange;
+}
+</style>
